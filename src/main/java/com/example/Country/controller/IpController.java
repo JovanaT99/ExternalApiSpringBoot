cf3a05dbb5f06ad0ae53d562cd1afc3866bc3d9e
Jovana Blagojevic <jovanablagojevic98@gmail.com>
2022-11-06 15:03:41 +0100

    update

--- src/main/java/com/example/Country/controller/IpController.java
+++ src/main/java/com/example/Country/controller/IpController.java
@@ -3,6 +3,7 @@ package com.example.Country.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,28 +13,29 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @AllArgsConstructor
+
 public class IpController {
     private final String uri = "http://ip-api.com/json";
     private final RestTemplate restTemplate;
 
-    @GetMapping("/me")
+    @GetMapping(value="/ip/me",produces= MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getMe() {
         try {
             String result = restTemplate.getForObject(uri, String.class);
             return new ResponseEntity<>(result, HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>("Jbg", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("Error", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @GetMapping(value = "{query}")
-    public String getByIp(@PathVariable("query") String query) {
+    @GetMapping(value = "/ip/{query}",produces= MediaType.APPLICATION_JSON_VALUE )
+    public ResponseEntity<?> getByIp(@PathVariable("query") String query) {
         try {
-            return restTemplate.getForObject(uri + "/" + query, String.class);
+            return ResponseEntity.ok().body(restTemplate.getForObject(uri + "/" + query, String.class));
         } catch (Exception e) {
             e.printStackTrace();
-            return String.valueOf(new IllegalArgumentException("Error"));
+            return new ResponseEntity<>("Error", HttpStatus.INTERNAL_SERVER_ERROR);
 
         }
     }
